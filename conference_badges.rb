7363a5d7f938a13fbcb509879d52a17a55d558d5
Steve Womble <swomble@aol.com>
2020-04-21 20:23:15 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,10 @@
-# Write your code here.
+def badge_maker(name)
+      "Hello, my name is #{name}."
+  end
+ 
+def batch_badge_creator(attendees)
+   array = []
+   attendees.each { |name| array << badge_maker(name) } 
+         array 
+end
+
